cfc765d9cdb3eb8777d179e5b92bceff5673cb19
Rishi Patni <rishipatni795@gmail.com>
2026-06-21 19:27:18 +0530

    Time: 1 ms (96.42%) | Memory: 42.6 MB (35.87%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfTwo(int n){
+
+        if (n <= 0){
+            return false;
+        }
+        while (n != 0){
+            int lastBit = n & 1;
+            n = n >> 1;
+            if (lastBit ==1 && n !=0 ){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
