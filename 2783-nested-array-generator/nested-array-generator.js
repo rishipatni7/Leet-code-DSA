03b2966db2fdce6e6d9cf23f94e5fa53d7280731
Rishi Patni <rishipatni795@gmail.com>
2026-06-26 00:01:36 +0530

    Time: 178 ms (21.86%) | Memory: 88.9 MB (69.77%) - LeetSync

---
+++ 2783-nested-array-generator/nested-array-generator.js
@@ -0,0 +1,10 @@
+
+var inorderTraversal = function* (arr) {
+    for (const element of arr) {
+        if (Array.isArray(element)) {
+            yield* inorderTraversal(element);
+        } else {
+            yield element;
+        }
+    }
+};
